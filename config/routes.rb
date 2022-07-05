a6b34dac15c1865f27ceefb7622c280beb9cacdd
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-05 13:00:06 -0400

    creates Songs app in rails

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  get "/songs" => "songs#index"
+
+  post "/songs" => "songs#create"
+
+  get "/songs/:id" => "songs#show"
+
+  patch "/songs/:id" => "songs#update"
+
+  delete "/songs/:id" => "songs#destroy"
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# Songs
+
+Song.create(title: "Kissing the Lipless", album: "Chutes Too Narrow", artist: "The Shins", year: 2003)
+Song.create(title: "Crawlspace", album: "This Dungeon Earth", artist: "Chat Pile", year: 2019)
+Song.create(title: "Ja Funmi", album: "Juju Music", artist: "King Sunny Ade", year: 1982)
+Song.create(title: "Lavatera", album: "In Her Garden", artist: "Colour Haze", year: 2017)
